8ee0ed76662b9757e4808b47cb6b0ecaf5cce282
chelms <charity@adobe.com>
2024-02-05 13:59:31 -0500

    2 - initial global styles added

--- scripts/aem.js
+++ scripts/aem.js
@@ -429,6 +429,9 @@ function decorateIcons(element, prefix = '') {
  */
 function decorateSections(main) {
   main.querySelectorAll(':scope > div').forEach((section) => {
+    // wrapping the section with section-outer for background styling
+    const sectionOuter = document.createElement('div');
+    sectionOuter.classList.add('section-outer');
     const wrappers = [];
     let defaultContent = false;
     [...section.children].forEach((e) => {
@@ -440,10 +443,13 @@ function decorateSections(main) {
       }
       wrappers[wrappers.length - 1].append(e);
     });
+    // wrap section content and its children in section-outer
+    sectionOuter.append(section);
+    main.append(sectionOuter);
     wrappers.forEach((wrapper) => section.append(wrapper));
     section.classList.add('section');
-    section.dataset.sectionStatus = 'initialized';
-    section.style.display = 'none';
+    sectionOuter.dataset.sectionStatus = 'initialized';
+    sectionOuter.style.display = 'none';
 
     // Process section metadata
     const sectionMeta = section.querySelector('div.section-metadata');
@@ -507,7 +513,7 @@ async function fetchPlaceholders(prefix = 'default') {
  * @param {Element} main The container element
  */
 function updateSectionsStatus(main) {
-  const sections = [...main.querySelectorAll(':scope > div.section')];
+  const sections = [...main.querySelectorAll(':scope > div.section-outer')];
   for (let i = 0; i < sections.length; i += 1) {
     const section = sections[i];
     const status = section.dataset.sectionStatus;
