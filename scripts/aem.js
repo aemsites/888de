411d4cbf4a20f755428535f1c4b210dc5a8638bd
chelms <charity@adobe.com>
2024-02-19 13:05:06 -0500

    linting

--- scripts/aem.js
+++ scripts/aem.js
@@ -399,7 +399,7 @@ function decorateButtons(element) {
  * @param {string} [prefix] prefix to be added to icon src
  * @param {string} [alt] alt text to be added to icon
  */
-function decorateIcon(span, prefix = '', alt = '') {
+function decorateIcon(span, prefix = '') {
   const iconName = Array.from(span.classList)
     .find((c) => c.startsWith('icon-'))
     .substring(5);
