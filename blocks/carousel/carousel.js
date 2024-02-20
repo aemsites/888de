280a118d0322377a202f9cb5c16602c969fd0e31
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-02-20 12:21:16 -0500

    fix

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-nested-ternary */
 import { readBlockConfig } from '../../scripts/aem.js';
 
 function createMobileSlide(slide, slideContent) {
-    slide.innerHTML = `<div class="brand-logo">
+  slide.innerHTML = `<div class="brand-logo">
     ${slideContent.icon.outerHTML || ''}
  </div>
  ${(slideContent.mobVideolink || slideContent.dtVideoLink) ? `<video class="mob" autoplay="autoplay" width="100%" loop="" muted="" playsinline="" poster="${slideContent.mobileImg ? slideContent.mobileImg : slideContent.img}">
@@ -15,7 +16,7 @@ function createMobileSlide(slide, slideContent) {
 }
 
 function populateDtSlidesWrapper(dtSlidesWrapper, slideContent) {
-    dtSlidesWrapper.innerHTML = `
+  dtSlidesWrapper.innerHTML = `
     ${slideContent.mobilePic ? slideContent.mobilePic.outerHTML : slideContent.pic.outerHTML}
     <div id="left">
        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="31.589" height="44.614" viewBox="0 0 31.589 44.614">
@@ -52,7 +53,7 @@ function populateDtSlidesWrapper(dtSlidesWrapper, slideContent) {
 }
 
 function createDesktopSlide(slide, slideContent) {
-    slide.innerHTML = `
+  slide.innerHTML = `
          <div class="brand-logo">
             ${slideContent.icon.outerHTML || ''}
             <div class="brand-logo-line">&nbsp;</div>
@@ -72,7 +73,7 @@ function createDesktopSlide(slide, slideContent) {
 }
 
 function createBoxOffer(boxOffer, slideContent) {
-    boxOffer.innerHTML = `
+  boxOffer.innerHTML = `
     ${slideContent.offer.outerHTML}
     ${slideContent.button.outerHTML}
     <div class="pc">${slideContent.termsLink.outerHTML}</div>
@@ -81,162 +82,159 @@ function createBoxOffer(boxOffer, slideContent) {
 
 // function to change slide on click of dot activating slide, dot and box offer
 function changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper) {
-    const slides = dtSlidesWrapper.querySelectorAll('.slide');
-    const dots = dtDotsWrapper.querySelectorAll('.dot');
-    const boxOffers = dtBannerBoxWrapper.querySelectorAll('.box-offer');
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
-    dots.forEach((dot) => {
-        dot.classList.remove('active');
-    });
-    boxOffers.forEach((boxOffer) => {
-        boxOffer.classList.remove('active');
-    });
-    console.log(slideIndex, slides[slideIndex - 1]);
-    slides[slideIndex - 1].classList.add('active');
-    dots[slideIndex - 1].classList.add('active');
-    boxOffers[slideIndex - 1].classList.add('active');
+  const slides = dtSlidesWrapper.querySelectorAll('.slide');
+  const dots = dtDotsWrapper.querySelectorAll('.dot');
+  const boxOffers = dtBannerBoxWrapper.querySelectorAll('.box-offer');
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  dots.forEach((dot) => {
+    dot.classList.remove('active');
+  });
+  boxOffers.forEach((boxOffer) => {
+    boxOffer.classList.remove('active');
+  });
+  console.log(slideIndex, slides[slideIndex - 1]);
+  slides[slideIndex - 1].classList.add('active');
+  dots[slideIndex - 1].classList.add('active');
+  boxOffers[slideIndex - 1].classList.add('active');
 }
 
 export default function decorate(block) {
-    const cols = [...block.firstElementChild.children];
-    const dtSlidesWrapper = document.createElement('div');
-    dtSlidesWrapper.classList.add('dt-slides-wrapper');
-    const dtDotsWrapper = document.createElement('div');
-    dtDotsWrapper.classList.add('dt-dots-wrapper');
-    const dtBannerBoxWrapper = document.createElement('div');
-    dtBannerBoxWrapper.classList.add('dt-banner-box-wrapper');
-    const mobileSlidesWrapper = document.createElement('div');
-    mobileSlidesWrapper.classList.add('mobile-slides-wrapper');
-    const configs = readBlockConfig(block);
-    const transitionDuration = configs['transition-duration'];
-    const slider = document.createElement('div');
-    slider.className = 'slider';
+  const dtSlidesWrapper = document.createElement('div');
+  dtSlidesWrapper.classList.add('dt-slides-wrapper');
+  const dtDotsWrapper = document.createElement('div');
+  dtDotsWrapper.classList.add('dt-dots-wrapper');
+  const dtBannerBoxWrapper = document.createElement('div');
+  dtBannerBoxWrapper.classList.add('dt-banner-box-wrapper');
+  const mobileSlidesWrapper = document.createElement('div');
+  mobileSlidesWrapper.classList.add('mobile-slides-wrapper');
+  const configs = readBlockConfig(block);
+  const transitionDuration = configs['transition-duration'];
+  const slider = document.createElement('div');
+  slider.className = 'slider';
 
-    // add index to each slide
-    let slideIndex = 1;
-    [...block.children].forEach((row) => {
-        let slideContent = {};
-        if (row.children.length === 2) {
-            if (row.children[0].innerText === 'transition duration') {
-                block.removeChild(row);
-                return;
-            }
+  // add index to each slide
+  let slideIndex = 1;
+  [...block.children].forEach((row) => {
+    const slideContent = {};
+    if (row.children.length === 2) {
+      if (row.children[0].innerText === 'transition duration') {
+        block.removeChild(row);
+        return;
+      }
+    }
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        slideContent.pic = pic;
+        const img = pic.querySelector('img');
+        slideContent.img = img.getAttribute('src');
+        const mobilePic = col.querySelectorAll('picture')[1];
+        if (mobilePic) {
+          slideContent.mobilePic = mobilePic;
+          const mobileImg = mobilePic.querySelector('img');
+          slideContent.mobileImg = mobileImg.getAttribute('src');
         }
-        [...row.children].forEach((col) => {
-            const pic = col.querySelector('picture');
-            if (pic) {
-                slideContent.pic = pic;
-                const img = pic.querySelector('img');
-                slideContent.img = img.getAttribute('src');
-                const mobilePic = col.querySelectorAll('picture')[1];
-                if (mobilePic) {
-                    slideContent.mobilePic = mobilePic;
-                    const mobileImg = mobilePic.querySelector('img');
-                    slideContent.mobileImg = mobileImg.getAttribute('src');
-                }
-                const dtVideoLink = col.querySelector('a');
-                if (dtVideoLink) {
-                    slideContent.dtVideoLink = dtVideoLink.getAttribute('href');
-                }
-                const mobVideolink = col.querySelectorAll('a')[1];
-                if (mobVideolink) {
-                    slideContent.mobVideolink = mobVideolink.getAttribute('href');
-                }
-
-            } else {
-                const icon = col.querySelector('span.icon');
-                if (icon) {
-                    slideContent.icon = icon;
-                    col.removeChild(icon.parentElement);
-                }
-                const button = col.querySelector('p.button-container');
-                if (button) {
-                    slideContent.button = button;
-                    col.removeChild(button);
-                }
-                const link = col.querySelector('a');
-                if (link) {
-                    link.classList.remove('button');
-                    slideContent.termsLink = link;
-                    col.removeChild(link.parentElement);
-                }
-                slideContent.offer = col;
-            }
-        });
-        const mobSlide = document.createElement('div');
-        mobSlide.className = `slide slide-${slideIndex}`;
-
-        // create mobile view
-        createMobileSlide(mobSlide, slideContent);
-        mobileSlidesWrapper.append(mobSlide);
-
-        // create desktop view
-        if (slideIndex === 1) {
-            populateDtSlidesWrapper(slider, slideContent);
+        const dtVideoLink = col.querySelector('a');
+        if (dtVideoLink) {
+          slideContent.dtVideoLink = dtVideoLink.getAttribute('href');
         }
-
-        const dtSlide = document.createElement('div');
-        dtSlide.className = `slide slide-${slideIndex}`;
-        if(slideIndex === 1) {
-            dtSlide.classList.add('active');
+        const mobVideolink = col.querySelectorAll('a')[1];
+        if (mobVideolink) {
+          slideContent.mobVideolink = mobVideolink.getAttribute('href');
         }
-        createDesktopSlide(dtSlide, slideContent);
-        slider.append(dtSlide);
-        dtSlidesWrapper.append(slider);
-
-        const dot = document.createElement('span');
-        dot.className = `dot dot-${slideIndex}`;
-        if(slideIndex === 1) {
-            dot.classList.add('active');
+      } else {
+        const icon = col.querySelector('span.icon');
+        if (icon) {
+          slideContent.icon = icon;
+          col.removeChild(icon.parentElement);
         }
-        dtDotsWrapper.append(dot);
-
-        const boxOffer = document.createElement('div');
-        boxOffer.className = `box-offer box-offer-${slideIndex}`;
-        if(slideIndex === 1) {
-            boxOffer.classList.add('active');
+        const button = col.querySelector('p.button-container');
+        if (button) {
+          slideContent.button = button;
+          col.removeChild(button);
         }
-        createBoxOffer(boxOffer, slideContent);
-        dtBannerBoxWrapper.append(boxOffer);
-
-        slideIndex++;
+        const link = col.querySelector('a');
+        if (link) {
+          link.classList.remove('button');
+          slideContent.termsLink = link;
+          col.removeChild(link.parentElement);
+        }
+        slideContent.offer = col;
+      }
     });
-    dtSlidesWrapper.append(dtDotsWrapper);
-    dtSlidesWrapper.append(dtBannerBoxWrapper);
-    block.replaceChildren(dtSlidesWrapper, mobileSlidesWrapper);
+    const mobSlide = document.createElement('div');
+    mobSlide.className = `slide slide-${slideIndex}`;
 
-    // call change slide on click of dot, click of left and right arrow, on hover of box offer
-    const dtLeft = dtSlidesWrapper.querySelector('#left');
-    const dtRight = dtSlidesWrapper.querySelector('#right');
-    dtLeft.addEventListener('click', () => {
-        slideIndex = slideIndex > 1 ? slideIndex - 1 : dtSlidesWrapper.querySelectorAll('.slide').length;
-        changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
-    });
-    dtRight.addEventListener('click', () => {
-        slideIndex = slideIndex < dtSlidesWrapper.querySelectorAll('.slide').length ? slideIndex + 1 : 1;
-        changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
-    });
-    dtDotsWrapper.addEventListener('click', (e) => {
-        slideIndex = parseInt(e.target.className.split(' ')[1].split('-')[1]);
-        changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
-    });
-    dtBannerBoxWrapper.addEventListener('mouseover', (e) => {
-        const boxOffer = e.target.closest('.box-offer');
-        slideIndex = parseInt(boxOffer.className.split(' ')[1].split('-')[2]);
-        changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
-    });
+    // create mobile view
+    createMobileSlide(mobSlide, slideContent);
+    mobileSlidesWrapper.append(mobSlide);
 
-    //auto rotate slides
-    const dtSlides = dtSlidesWrapper.querySelectorAll('.slide');
-    slideIndex = 1;
-    setInterval(() => {
-        if (slideIndex > dtSlides.length) {
-            slideIndex = 1;
-        }
-        changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
-        slideIndex++;
-    }, transitionDuration*1000);
+    // create desktop view
+    if (slideIndex === 1) {
+      populateDtSlidesWrapper(slider, slideContent);
+    }
 
+    const dtSlide = document.createElement('div');
+    dtSlide.className = `slide slide-${slideIndex}`;
+    if (slideIndex === 1) {
+      dtSlide.classList.add('active');
+    }
+    createDesktopSlide(dtSlide, slideContent);
+    slider.append(dtSlide);
+    dtSlidesWrapper.append(slider);
+
+    const dot = document.createElement('span');
+    dot.className = `dot dot-${slideIndex}`;
+    if (slideIndex === 1) {
+      dot.classList.add('active');
+    }
+    dtDotsWrapper.append(dot);
+
+    const boxOffer = document.createElement('div');
+    boxOffer.className = `box-offer box-offer-${slideIndex}`;
+    if (slideIndex === 1) {
+      boxOffer.classList.add('active');
+    }
+    createBoxOffer(boxOffer, slideContent);
+    dtBannerBoxWrapper.append(boxOffer);
+
+    slideIndex += 1;
+  });
+  dtSlidesWrapper.append(dtDotsWrapper);
+  dtSlidesWrapper.append(dtBannerBoxWrapper);
+  block.replaceChildren(dtSlidesWrapper, mobileSlidesWrapper);
+
+  // call change slide on click of dot, click of left and right arrow, on hover of box offer
+  const dtLeft = dtSlidesWrapper.querySelector('#left');
+  const dtRight = dtSlidesWrapper.querySelector('#right');
+  dtLeft.addEventListener('click', () => {
+    slideIndex = slideIndex > 1 ? slideIndex - 1 : dtSlidesWrapper.querySelectorAll('.slide').length;
+    changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
+  });
+  dtRight.addEventListener('click', () => {
+    slideIndex = slideIndex < dtSlidesWrapper.querySelectorAll('.slide').length ? slideIndex + 1 : 1;
+    changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
+  });
+  dtDotsWrapper.addEventListener('click', (e) => {
+    slideIndex = parseInt(e.target.className.split(' ')[1].split('-')[1], 10);
+    changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
+  });
+  dtBannerBoxWrapper.addEventListener('mouseover', (e) => {
+    const boxOffer = e.target.closest('.box-offer');
+    slideIndex = parseInt(boxOffer.className.split(' ')[1].split('-')[2], 10);
+    changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
+  });
+
+  // auto rotate slides
+  const dtSlides = dtSlidesWrapper.querySelectorAll('.slide');
+  slideIndex = 1;
+  setInterval(() => {
+    if (slideIndex > dtSlides.length) {
+      slideIndex = 1;
+    }
+    changeSlide(slideIndex, dtSlidesWrapper, dtDotsWrapper, dtBannerBoxWrapper);
+    slideIndex += 1;
+  }, transitionDuration * 1000);
 }
