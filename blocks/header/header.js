2e295bf024aa4524918231623d602f51afb336cf
Dave Fink <138233107+dave-fink@users.noreply.github.com>
2024-02-22 15:23:45 -0500

    latest changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -4,7 +4,6 @@ import { nav, div, span, a, img, button, i } from '../../scripts/dom-helpers.js'
 export default async function decorate(block) {
   const fetchNav = await fetch('/drafts/dfink/nav.plain.html');
   const navHTML = await fetchNav.text();
-  const navTransitionTime = 400; // match --nav-transition-time var in styles.css
   const $nav = nav();
   $nav.innerHTML = navHTML;
   const $navUL = $nav.querySelector('ul');
@@ -12,10 +11,12 @@ export default async function decorate(block) {
 
   $accordionLIs.forEach((li) => {
     // if li has ul, format it as accordion
-    if (li.querySelector('ul') !== null) {
-      const spanLI = span(li.firstChild.textContent.trim());
+    const nestedUl = li.querySelector('ul');
+    if (nestedUl) {
+      const liText = li.firstChild.textContent.trim();
+      const $spanLI = span(liText);
       const $arrow = i('>');
-      li.firstChild.replaceWith(spanLI, $arrow);
+      li.firstChild.replaceWith($spanLI, $arrow);
 
       li.addEventListener('click', () => {
         li.classList.toggle('open');
@@ -25,8 +26,9 @@ export default async function decorate(block) {
 
   const $body = document.body;
   const $main = document.querySelector('main');
-  const $navBtn = div({ class: 'nav-btn' }, span(), span(), span());
 
+  const navTransitionTime = 400; // match --nav-transition-time var in styles.css
+  const $navBtn = div({ class: 'nav-btn' }, span(), span(), span());
   $main.addEventListener('click', () => {
     $body.classList.remove('nav-open');
     setTimeout(() => {
