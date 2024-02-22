43b708c2571fe0f9c9bd335e1730202c46286b33
Dave Fink <138233107+dave-fink@users.noreply.github.com>
2024-02-22 15:38:43 -0500

    Latest

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,52 +1,36 @@
-/* eslint-disable object-curly-newline */
 import { nav, div, span, a, img, button, i } from '../../scripts/dom-helpers.js';
+
+function closeNav($body, navTransitionTime) {
+  $body.classList.remove('nav-open');
+  setTimeout(() => {
+    $body.classList.remove('no-scroll');
+  }, navTransitionTime);
+}
 
 export default async function decorate(block) {
   const fetchNav = await fetch('/drafts/dfink/nav.plain.html');
   const navHTML = await fetchNav.text();
   const $nav = nav();
   $nav.innerHTML = navHTML;
-  const $navUL = $nav.querySelector('ul');
-  const $accordionLIs = Array.from($navUL.children);
-
-  $accordionLIs.forEach((li) => {
-    // if li has ul, format it as accordion
-    const nestedUl = li.querySelector('ul');
-    if (nestedUl) {
-      const liText = li.firstChild.textContent.trim();
-      const $spanLI = span(liText);
-      const $arrow = i('>');
-      li.firstChild.replaceWith($spanLI, $arrow);
-
-      li.addEventListener('click', () => {
-        li.classList.toggle('open');
-      });
-    }
-  });
 
   const $body = document.body;
   const $main = document.querySelector('main');
+  const $header = document.querySelector('header');
 
-  const navTransitionTime = 400; // match --nav-transition-time var in styles.css
+  // nav burger menu
   const $navBtn = div({ class: 'nav-btn' }, span(), span(), span());
-  $main.addEventListener('click', () => {
-    $body.classList.remove('nav-open');
-    setTimeout(() => {
-      $body.classList.remove('no-scroll');
-    }, navTransitionTime);
-  });
-
+  const navTransitionTime = 400; // match --nav-transition-time var in styles.css
   $navBtn.addEventListener('click', () => {
     if (!$body.classList.contains('nav-open')) {
       $body.classList.add('nav-open');
       $body.classList.add('no-scroll');
     } else {
-      $body.classList.remove('nav-open');
-      // allow animation to finish
-      setTimeout(() => {
-        $body.classList.remove('no-scroll');
-      }, navTransitionTime);
+      closeNav($body, navTransitionTime);
     }
+  });
+
+  $main.addEventListener('click', () => {
+    closeNav($body, navTransitionTime);
   });
 
   const $logo = a({ class: 'logo', href: '/' }, '888.de', img({
@@ -60,5 +44,22 @@ export default async function decorate(block) {
 
   block.replaceWith($navBtn, $logo, $login);
 
-  document.querySelector('header').after($nav);
+  $header.after($nav);
+
+  // accordion functionality
+  const $navUL = $nav.querySelector('ul');
+  const $accordionLIs = Array.from($navUL.children);
+  $accordionLIs.forEach((li) => {
+    const nestedUl = li.querySelector('ul');
+    if (nestedUl) {
+      const liText = li.firstChild.textContent.trim();
+      const $spanLI = span(liText);
+      const $arrow = i('>');
+      li.firstChild.replaceWith($spanLI, $arrow);
+
+      li.addEventListener('click', () => {
+        li.classList.toggle('open');
+      });
+    }
+  });
 }
