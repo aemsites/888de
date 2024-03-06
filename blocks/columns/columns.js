c6080a048c48060a210aaf95fd550c8aad62dffc
kailasnadh790 <53608019+kailasnadh790@users.noreply.github.com>
2024-03-06 16:48:52 -0500

    clubspage

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,6 +2,14 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  const isSectionCol = block.classList.contains('col-10-90');
+  if (isSectionCol) {
+    const imgs = block.querySelectorAll('img');
+    imgs.forEach((img) => {
+      img.style.width = `${img.width}px`;
+    });
+  }
+  
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
