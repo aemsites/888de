468a89b12b82d30d95e362faafcbbbdf7385f1df
dave-fink <--global>
2024-03-11 15:50:37 -0400

    moving script placement

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -17,7 +17,7 @@ function loadGTM() {
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.setAttribute('class','optanon-category-C0001');j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-NSNT2WJ');`;
-  document.head.prepend(scriptTag);
+  document.head.append(scriptTag);
 }
 
 loadGTM();
