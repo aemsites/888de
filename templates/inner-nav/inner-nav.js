190f8e5451e887ba33cd9ff25919a7cf737476e8
dave-fink <david@keypixel.com>
2024-03-18 11:07:14 -0400

    updating styles

--- templates/inner-nav/inner-nav.js
+++ templates/inner-nav/inner-nav.js
@@ -5,7 +5,6 @@ import { breadcrumbs } from '../../scripts/scripts.js';
 
 export default async function decorate(doc) {
   const $anchorNav = ul({ class: 'inner-nav' });
-
   const parentFolder = window.location.pathname.split('/').filter(Boolean)[0];
 
   fetch('/query-index.json')
@@ -14,17 +13,15 @@ export default async function decorate(doc) {
       // get paths that match parent & exclude 'index-breadcrumb'
       const filteredPaths = data.data.filter((item) => item.path.startsWith(`/${parentFolder}`) && !item.path.includes('index-breadcrumb'));
 
-      // sort by 'nav-order''
+      // sort by 'nav-order'
       filteredPaths.sort((A, B) => parseInt(A['nav-order'], 10) - parseInt(B['nav-order'], 10));
 
       const listItems = filteredPaths.map((item) => {
         const isActive = item.path === window.location.pathname ? 'active' : '';
-        const listItem = li({ class: isActive }, a({ href: item.path }, item.title.replace(/\s*\|\s*888\.de$/, '')));
-        return listItem;
+        return li({ class: isActive }, a({ href: item.path }, item.title.replace(/\s*\|\s*888\.de$/, '')));
       });
 
-      // eslint-disable-next-line no-shadow
-      listItems.forEach((li) => $anchorNav.appendChild(li));
+      listItems.forEach((listItem) => $anchorNav.appendChild(listItem));
     })
     .catch((error) => console.error('Error fetching JSON:', error));
 
