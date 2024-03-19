13eead1f13a339d29bee40f1b7a0079c8397424e
dave-fink <david@keypixel.com>
2024-03-19 12:49:06 -0400

    fixing issues

--- templates/inner/inner.js
+++ templates/inner/inner.js
@@ -2,13 +2,12 @@ import { div } from '../../scripts/dom-helpers.js';
 import { breadcrumbs } from '../../scripts/scripts.js';
 
 export default async function decorate(doc) {
-  const $page = doc.querySelector('main > .section-outer > .section');
-
-  // insert breadcrumbs
+  const $sectionContent = doc.querySelector('main > .section-outer .section');
   const $breadcrumbsContainer = div({ class: 'breadcrumbs-container' });
-  $page.prepend($breadcrumbsContainer);
 
-  breadcrumbs(doc, $page).then(($breadcrumbs) => {
+  $sectionContent.prepend($breadcrumbsContainer);
+
+  breadcrumbs(doc).then(($breadcrumbs) => {
     $breadcrumbsContainer.append($breadcrumbs);
   }).catch((error) => {
     console.error('Error generating breadcrumbs:', error);
