3d2837194053126bd543b2b15c680bf468a96ca0
dave-fink <david@keypixel.com>
2024-04-04 14:44:07 -0400

    removing var

--- scripts/888-main.js
+++ scripts/888-main.js
@@ -5,9 +5,6 @@
 /* eslint-disable no-useless-concat */
 /* eslint-disable func-names */
 
-//  add sCut
-let sCut; // added b/c sCut was not defined & throwing console errors
-
 window.addQueryParameter = (src) => {
   if (!src.href.includes(sCut)) {
     src.href += (src.href.includes('?') ? '&' : '?') + sCut;
