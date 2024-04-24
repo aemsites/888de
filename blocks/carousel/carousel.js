78c13adcbcf59f7c03a69ea6088c5e6e19e74dd7
Kailas Nadh U <53608019+kailasnadh790@users.noreply.github.com>
2024-04-24 17:44:51 -0400

    Lhs/carousel (#241)
    * Update scripts.js
    
    * Update carousel.js
    
    * Update carousel.js
    
    * Update carousel.js
    
    * Update carousel.js
    
    * Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -46,15 +46,12 @@ function createDesktopSlide(slide, slideContent) {
             ${slideContent.button ? slideContent.button.outerHTML : ''}
             <div class="banner-terms">${slideContent.termsLink ? slideContent.termsLink.outerHTML : ''}</div>
          </div>`;
-  setTimeout(() => {
-    slide.innerHTML += `
+  slide.innerHTML += `
          ${(slideContent.dtVideoLink) ? `
          <video class="mob" autoplay="autoplay" width="100%" loop="" muted="" playsinline="" poster="${slideContent.img ? slideContent.img : ''}">
             <source src="${slideContent.dtVideoLink ? slideContent.dtVideoLink : ''}" type="video/mp4">
         </video>` : (slideContent.pic ? slideContent.pic.outerHTML : '')}
     `;
-    slide.parentElement.querySelector('img').style.visibility = 'hidden';
-  }, 3500);
 }
 
 function createBoxOffer(boxOffer, slideContent) {
@@ -100,7 +97,7 @@ export default function decorate(block) {
   let mobRatio;
   const vpWidth = window.innerWidth;
 
-  const firstPic = block.querySelector('picture').cloneNode(true);
+  const firstPic = block.querySelectorAll('picture')[0];
   firstPic.querySelector('img').setAttribute('loading', 'eager');
   populateDtSlidesWrapper(slider, firstPic);
   let slideIndex = 1;
